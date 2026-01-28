1fa8bfcf7740cb60e732f414887a64047bba9b47
Felix Lenner <felix.lenner@scilifelab.se>
2026-01-28 11:19:28 +0100

    Parallel threads caused instable FASTQs

--- make_test_data.sh
+++ make_test_data.sh
@@ -124,7 +124,8 @@ prepare_bam 35 lr:hq data/hg002.haplotagged.bam testdata/HG002_ONT_copy.bam
 
 # Make fastq
 samtools sort -n testdata/HG002_PacBio_Revio.bam | samtools fastq -T \* -@ 36 | pigz -p 36 > testdata/HG002_PacBio_Revio.fastq.gz
-samtools sort -n testdata/HG002_ONT.bam | samtools fastq -T \* -@ 36 testdata/HG002_ONT.bam|chopper --maxlength 200000 | pigz -p 36 > testdata/HG002_ONT.fastq.gz
+# chopper --threads 1 needed for stable output
+samtools sort -n testdata/HG002_ONT.bam | samtools fastq -T \* -@ 36 testdata/HG002_ONT.bam|chopper --maxlength 200000 --threads 1 | pigz -p 36 > testdata/HG002_ONT.fastq.gz
 
 # 6. Create a SVDB file from https://zenodo.org/records/11511513/files/CoLoRSdb.GRCh38.v1.0.0.pbsv.jasmine.vcf.gz
 bcftools view -R tmp/test_somalier_small.bed  data/CoLoRSdb.GRCh38.v1.0.0.pbsv.jasmine.vcf.gz --output-type z --no-version --output tmp/colorsdb.test_data.vcf.gz
